2794f287be4782678031b4dec434a4b1456727b5
Aleksandr Nikitin <NAI_surgut@mail.ru>
2025-03-25 14:23:46 +0700

    contacts

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,9 +3,12 @@ from tkinter.font import names
 from django.urls import path, include
 from catalog.apps import CatalogConfig
 from catalog.views import home
+from catalog.views import contacts
+
 catalog_name = CatalogConfig.name
 app_name = 'catalog'
 urlpatterns = [
 
-    path('', home, name='home')
-]
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts')
+    ]
